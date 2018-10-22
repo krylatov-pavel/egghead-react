f6f807d5a872e9459ce5b4216de7029217c5b72f
Pavel Krylatov <krylatov.pavel@gmail.com>
2018-10-22 16:16:25 +0300

    add middlewares;

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,48 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import promise from 'redux-promise';
 import { todos } from './todos';
 import * as fromTodos from './todos';
 
-const addLoggingToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
-
-    if (console.group) {
-        return (action) => {
-            console.group(action.type);
-            console.log('%c prev state ', 'color: gray', store.getState());
-            console.log('%c action ', 'color: blue', action.type);
-            const returnValue = rawDispatch(action);
-            console.log('%c next state ', 'color: green', store.getState());
-            console.groupEnd(action.type);
-
-            return returnValue;
-        }
-    } else {
-        return rawDispatch;
-    }
-};
-
-const addPromiseSupportToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
-
-    return (action) => {
-        if (typeof action.then === 'function') {
-            return action.then(rawDispatch);
-        }
-        else {
-            return rawDispatch(action);
-        }
-    }
-}
-
 export default () => {
-    const store = createStore(combineReducers({
-        todos: todos
-    }));
+    const middlewares = [promise, logger];
 
-    store.dispatch = addLoggingToDispatch(store);
-    store.dispatch = addPromiseSupportToDispatch(store);
-
-    return store;
+    return createStore(
+        combineReducers({
+            todos: todos
+        }),
+        applyMiddleware(...middlewares)
+    );
 };
 
 export const getVisibleToDos = (state, filter) => fromTodos.getVisibleToDos(state.todos, filter);
