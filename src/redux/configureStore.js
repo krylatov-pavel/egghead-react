37597dbe3785254920f878b143b1ea94a1be61cd
Pavel Krylatov <krylatov.pavel@gmail.com>
2018-10-22 13:26:29 +0300

    add logging;

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,33 @@ import { createStore, combineReducers } from 'redux';
 import { todos } from './todos';
 import * as fromTodos from './todos';
 
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+
+    if (console.group) {
+        return (action) => {
+            console.group(action.type);
+            console.log('%c prev state ', 'color: gray', store.getState());
+            console.log('%c action ', 'color: blue', action.type);
+            const returnValue = rawDispatch(action);
+            console.log('%c next state ', 'color: green', store.getState());
+            console.groupEnd(action.type);
+
+            return returnValue;
+        }
+    } else {
+        return rawDispatch;
+    }
+}
+
 export default () => {
-    return createStore(combineReducers({
+    const store = createStore(combineReducers({
         todos: todos
     }));
+
+    store.dispatch = addLoggingToDispatch(store);
+
+    return store;
 };
 
 export const getVisibleToDos = (state, filter) => fromTodos.getVisibleToDos(state.todos, filter);
