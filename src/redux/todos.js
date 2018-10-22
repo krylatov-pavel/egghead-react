97dc24cdaa2aeb0643af5e609af7d2c2b4876511
Pavel Krylatov <krylatov.pavel@gmail.com>
2018-10-22 13:09:07 +0300

    normalize todos;

--- src/redux/todos.js
+++ src/redux/todos.js
@@ -1,25 +1,45 @@
+import { combineReducers } from 'redux';
 import * as ActionTypes from './actionTypes';
 import todo from './todo';
 
-export const todos = (state = [], action) => {
+const byId = (state = {}, action) => {
     switch (action.type) {
         case ActionTypes.ADD_TODO:
-            return state.concat(todo(undefined, action));
         case ActionTypes.TOGGLE_TODO:
-            return state.map(e => todo(e, action));
+            return {
+                ...state,
+                [action.id]: todo(state[action.id], action)
+            }
         default:
             return state;
     }
 };
 
-export const getVisibleToDos = (todos, filter) => {
+const allIds = (state = [], action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_TODO:
+            return state.concat(action.id);
+        default:
+            return state;
+    }
+};
+
+export const todos = combineReducers({
+    byId: byId,
+    allIds: allIds
+});
+
+const getAllTodos = (state) => state.allIds.map((id) => state.byId[id]);
+
+export const getVisibleToDos = (state, filter) => {
+    const allTodos = getAllTodos(state)
     switch (filter) {
         case 'all':
-            return todos;
+            return allTodos;
         case 'active':
-            return todos.filter(todo => todo.active);
+            return allTodos.filter(todo => todo.active);
         case 'completed':
-            return todos.filter(todo => !todo.active);
+            return allTodos.filter(todo => !todo.active);
         default:
             throw new Error('invalid todo filter type');
     }
